61f2a9cc38386cd29ae7518cfe1671bdae0cb189
emon1432 <ovirajemon143@gmail.com>
2023-06-07 00:24:16 +0600

    update project and create some seed

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -7,6 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Orangehill\\Iseed' => array($vendorDir . '/orangehill/iseed/src'),
-    'Mockery' => array($vendorDir . '/mockery/mockery/library'),
     'Detection' => array($vendorDir . '/mobiledetect/mobiledetectlib/namespaced'),
 );
